31d155e7a4f8f4a7a2da08e518e4005ab49918cb
leila_ilkhani <work@leilynn.link>
2024-11-29 02:30:45 +0330

    feat(routes): add global layout, and delayed mounting for enter animations to work

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import '../app.css';
+
 	let { children } = $props();
+
+	let visible = $state(false);
+	onMount(() => {
+		setTimeout(() => {
+			visible = true;
+		}, 700);
+	});
 </script>
 
-{@render children()}
+<div class="flex min-h-screen w-full flex-col items-center justify-center">
+	<main class="flex w-96 max-w-screen-sm flex-col gap-12 px-3">
+		{#if visible}
+			{@render children()}
+		{:else}
+			<h1 class="-space-x-5 text-center text-5xl font-bold text-white">
+				<span class="animate-duration-700 animate-pulse">.</span>
+				<span class="animate-delay-100 animate-duration-700 animate-pulse">.</span>
+				<span class="animate-delay-200 animate-duration-700 animate-pulse">.</span>
+			</h1>
+		{/if}
+	</main>
+</div>
